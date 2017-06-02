38c9227cf69e8fb654a6fb7cda0228e7c9ec071b
crossjs <liwenfu@crossjs.com>
2017-06-02 17:42:02 +0800

    update script: mirror

--- template/cli/mirror.js
+++ template/cli/mirror.js
@@ -1,24 +1,22 @@
-require('babel-register')
-
 const fs = require('fs')
-const argv = require('yargs').argv
-const paths = require('../config').paths
+const resolve = require('path').resolve
 
+const registry = process.argv[3]
 const re = /https?:\/\/registry\.[a-z.]+/g
 
 function replaceLock (cb) {
-  const lock = paths.base('npm-shrinkwrap.json')
+  const lock = resolve(__dirname, '../npm-shrinkwrap.json')
   fs.readFile(lock, (err, buf) => {
     if (err) {
       console.error(err)
     } else {
       let content = buf.toString()
-      content = content.replace(re, argv.registry)
+      content = content.replace(re, registry)
       fs.writeFile(lock, content, err => {
         if (err) {
           console.error(err)
         } else {
-          console.log(`registries in "npm-shrinkwrap.json" were replaced with "${argv.registry}"`)
+          console.log(`registries in "npm-shrinkwrap.json" were replaced with "${registry}"`)
           cb()
         }
       })
@@ -27,18 +25,18 @@ function replaceLock (cb) {
 }
 
 function replaceRc () {
-  const rc = paths.base('.npmrc')
+  const rc = resolve(__dirname, '../.npmrc')
   fs.readFile(rc, (err, buf) => {
     if (err) {
       console.error(err)
     } else {
       let content = buf.toString()
-      content = content.replace(re, argv.registry)
+      content = content.replace(re, registry)
       fs.writeFile(rc, content, err => {
         if (err) {
           console.error(err)
         } else {
-          console.log(`registries in ".npmrc" were replaced with "${argv.registry}"`)
+          console.log(`registries in ".npmrc" were replaced with "${registry}"`)
         }
       })
     }
